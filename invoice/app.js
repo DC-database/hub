07fc44df5aeefff7f242465afa9907d2ff855d38
DC-database <iba.dc00117@gmail.com>
2025-11-12 14:26:14 +0300

    Add files via upload

--- invoice/app.js
+++ invoice/app.js
@@ -1,5 +1,5 @@
 // --- ADD THIS LINE AT THE VERY TOP OF APP.JS ---
-const APP_VERSION = "3.1.2"; // You can change "1.1.0" to any version you want
+const APP_VERSION = "3.1.3"; // You can change "1.1.0" to any version you want
 
 // --- 1. FIREBASE CONFIGURATION & 2. INITIALIZE FIREBASE ---
 // Main DB for approvers, job_entries, project_sites
