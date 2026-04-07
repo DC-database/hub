391421ae382e4521301e8c18d4e0cea05f70c772
DC-database <iba.dc00117@gmail.com>
2026-04-07 11:35:27 +0300

    Add files via upload

--- invoice/app.js
+++ invoice/app.js
@@ -6,7 +6,7 @@
 */
 
 // app.js - Top of file
-const APP_VERSION = "6.6.7";
+const APP_VERSION = "6.6.8";
 
 // ======================================================================
 // ULTRA-FAST AUDIO ENGINE (WITH CONFIRM SOUND & SNAP-SHUT LOCK)
